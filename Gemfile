1670ca0a4c1cf116232eccb2d49c352d4f93aebd
Philip Broadway <me@philipbroadway.com>
2023-05-30 21:08:01 -0600

    save

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'rest-client'
+gem 'colorize'
+gem 'optparse'
+gem 'tty-spinner'
+gem 'terrapin'
+gem 'octokit'
+gem 'excon'
+gem 'tmpdir'
+gem 'git'
+gem 'fileutils'
--- evalgpt.rb
+++ evalgpt.rb
@@ -6,14 +6,17 @@ require 'tty-spinner'
 require 'terrapin'
 require 'pty'
 require 'excon'
+require 'octokit'
+require 'git'
 
 # MODEL='gpt-3.5-turbo-0301'
-MODEL='gpt-3.5-turbo'
+MODEL = 'gpt-3.5-turbo'
+# MODEL='gpt-4'
 API_URL = 'https://api.openai.com/v1/chat/completions'
 class EvalGPT
-  SUPPORTED_LANGUAGES = ['text','ruby', 'javascript', 'python', 'swift', 'bash', 'node']
-  SUPPORTED_EXTENSIONS = ['txt','rb', 'js', 'py', 'swift', 'sh', 'js']
-  
+  SUPPORTED_LANGUAGES = %w[text ruby javascript python swift bash node]
+  SUPPORTED_EXTENSIONS = %w[txt rb js py swift sh js]
+
   def initialize(api_key, verbose, input = nil, output_folder = nil)
     @selected_model = MODEL
     @api_key = api_key
@@ -38,51 +41,54 @@ class EvalGPT
 
   def chat
     if @in && @out
-    input = ""
-    puts "\[prommpt] ".colorize(:green) + @in.colorize(:red) + ":\n"
-    puts "```"
+      input = ''
+      puts "\[prommpt] ".colorize(:green) + @in.colorize(:red) + ":\n"
+      puts '```'
       open @in do |f|
         f.each_line do |line|
           input = "#{input}#{line}"
           puts "#{line}"
         end
       end
-    puts "```"
+      puts '```'
       @messages << {
         'role' => 'user',
         'content' => 'Always respond with text format that indates path if relevant, filename (markdown bold) a new line then content for the file(s) the user will create. Ex: **file_1**\n```\n content\n```  **file_2**\n```\n content\n``` ' + input
       }
-      
+
       puts "\n"
 
       puts "\n"
       response = stream_chatgpt
-      #response = call_chatgpt
+      # response = call_chatgpt
       @spinner.stop('[response parsed]')
-      puts ""
+      puts ''
       puts response
-      puts ""
+      puts ''
       response = extract_code(response)
       puts "\n\extract_code: #{response}\n\n"
+      locations = []
       if response
         response.each do |k|
           puts k
           key = k[0]
           v = k[1]
           detected = detect_language(v) || 'text'
-          puts "k: #{key} v:#{v}"
+          # puts "k: #{key} v:#{v}"
           write_code(v, detected, key)
+          locations.push(key)
         end
       else
-        puts "No code response found"
+        puts 'No code response found'
       end
-      return
+      commit_to_pull_request(locations, Time.now.strftime('%Y-%m-%d_%H-%M-%S'))
     end
     help
     loop do
       print 'User: '.colorize(:blue)
       user_message = gets.to_s.chomp
       break if user_message.downcase == 'exit'
+
       if user_message.downcase == 'select_model'
         select_model
         chat
@@ -104,41 +110,76 @@ class EvalGPT
       @spinner.stop("[response]: #{response}")
 
       if @verbose
-        puts ""
-        puts response&.colorize(:gray) 
-        puts ""
+        puts ''
+        puts response&.colorize(:gray)
+        puts ''
       end
-      
-      code_response = response#extract_code(response)
-      if code_response
-        puts ""
-        puts "Language detected: ".colorize(:white) + language&.colorize(:pink)
-        puts ""
-        puts code_response&.colorize(:green)
-        puts ""
-        print "Evaluate code with (#{SUPPORTED_LANGUAGES.join('/')}/no): ".colorize(:white)
-        evaluate = gets.chomp.downcase
-        if evaluate != 'no'
-          new_language = evaluate != 'yes' ? evaluate : language
-          begin
-            eval_result = execute_code(code_response, new_language)
-            puts "#{eval_result}"&.colorize(:yellow)
-          rescue Exception => e
-            puts "An error occurred while evaluating the code: #{e}".colorize(:red)
-          end
-        end
+
+      code_response = response # extract_code(response)
+      next unless code_response
+
+      puts ''
+      puts 'Language detected: '.colorize(:white) + language&.colorize(:pink)
+      puts ''
+      puts code_response&.colorize(:green)
+      puts ''
+      print "Evaluate code with (#{SUPPORTED_LANGUAGES.join('/')}/no): ".colorize(:white)
+      evaluate = gets.chomp.downcase
+      next unless evaluate != 'no'
+
+      new_language = evaluate != 'yes' ? evaluate : language
+      begin
+        eval_result = execute_code(code_response, new_language)
+        puts "#{eval_result}"&.colorize(:yellow)
+      rescue Exception => e
+        puts "An error occurred while evaluating the code: #{e}".colorize(:red)
       end
     end
   end
 
+  def commit_to_pull_request(_files, now)
+    token = ENV['GH_ACCESS_TOKEN']
+    client = Octokit::Client.new(access_token: token)
+    user = client.user
+    user.login
+    repo = 'frontdesk/output'
+
+      begin
+        puts "git clone https://frontdesk:#{token}@github.com/#{repo} repo"
+        `git clone https://frontdesk:#{token}@github.com/#{repo} repo`
+        # line = Terrapin::CommandLine.new("")
+        # line.run
+        name = now
+        puts `ls -GA`
+        # absolute_path = File.expand_path("repo")
+        # puts "absolute_path: #{absolute_path}"
+        # line = Terrapin::CommandLine.new("cd", absolute_path)
+        # line.run
+        puts `pwd`
+        `cd repo`
+        `git checkout -b #{name}`
+
+        `cp -r ../output/. .`
+
+        `git add .`
+
+        `git commit -m "#{@messages.join(',')}"`
+
+        `git push origin #{name}`
+      rescue Octokit::Error => e
+        puts "An error occurred while creating the pull request: #{e.message}"
+      end
+
+  end
+
   def write_code(code, language, filename = nil)
     puts "write_code: language: #{language} filename: #{filename}"
-    timestamp = Time.now.strftime("%Y-%m-%d_%H-%M-%S")
+    timestamp = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
     ext = SUPPORTED_EXTENSIONS[SUPPORTED_LANGUAGES.index(language)]
-    filename = "#{language}_#{timestamp}_#{Time.now.to_i}.#{ext}" unless filename
+    filename ||= "#{language}_#{timestamp}_#{Time.now.to_i}.#{ext}"
     puts "ext: #{ext} filename: #{filename}"
     if @out
-      
+
       full_path = File.join(@out, filename)
       dir = File.dirname(full_path)
       create_directory_if_not_exists(@out)
@@ -146,17 +187,15 @@ class EvalGPT
       paths = full_path.split('/')
       paths.pop
       paths = paths.join('/')
-      if paths
-        FileUtils.mkdir_p(paths) unless File.directory?(paths)
-      end
+      FileUtils.mkdir_p(paths) if paths && !File.directory?(paths)
       puts ":write: #{full_path}"
-      File.open(full_path, "w") do |file|
+      File.open(full_path, 'w') do |file|
         file.write(code)
       end
-      return full_path
+      full_path
     else
       create_directory_if_not_exists('output')
-      File.open( "output/#{filename}", "w") do |file|
+      File.open("output/#{filename}", 'w') do |file|
         file.write(code)
         return "#{Dir.pwd}/output/#{filename}"
       end
@@ -165,34 +204,31 @@ class EvalGPT
 
   def execute_code(code, language)
     location = write_code(code, language)
-    lang = Terrapin::CommandLine.new("which", language == 'javascript' ? 'node' : language)
+    lang = Terrapin::CommandLine.new('which', language == 'javascript' ? 'node' : language)
     lang = lang.run
-    puts ""
-    puts "File saved to: ".colorize(:white) + location.colorize(:red)
-    puts ""
+    puts ''
+    puts 'File saved to: '.colorize(:white) + location.colorize(:red)
+    puts ''
     case language
     when 'ruby'
       eval(code)
     when 'python', 'swift', 'javascript', 'bash', 'node'
       $stdin.sync = true
-      PTY.spawn("#{language == 'javascript' ? 'node' : language}", "#{location}") do |stdout, stdin, pid|
-      input_thread = Thread.new do
-        begin
+      PTY.spawn("#{language == 'javascript' ? 'node' : language}", "#{location}") do |stdout, stdin, _pid|
+        input_thread = Thread.new do
           while line = $stdin.gets
             stdin.puts(line)
           end
         rescue Errno::EIO
-
         end
-      end
 
-      stdout.each do |line|
-        print line
-        $stdout.flush
-      end
+        stdout.each do |line|
+          print line
+          $stdout.flush
+        end
 
-      input_thread.join
-    end
+        input_thread.join
+      end
 
     else
       raise "Unsupported language: #{language}"
@@ -200,27 +236,26 @@ class EvalGPT
   end
 
   def help
-    ascii = """
+    ascii = ''"
 ███████ ██    ██  █████  ██       ██████  ██████  ████████ 
 ██      ██    ██ ██   ██ ██      ██       ██   ██    ██    
-█████   ██    ██ ███████ ██      ██   ███ ██████     ██    
-██       ██  ██  ██   ██ ██      ██    ██ ██         ██    
+█████   ██    ██ ███████ ██      ██   ███ ██████     ██ 
+██       ██  ██  ██   ██ ██      ██    ██ ██         ██ 
 ███████   ████   ██   ██ ███████  ██████  ██         ██ 
-    """
-    puts ascii.colorize(:pink)
-    puts "Options:"
-    puts ""
+    "''
+    puts ascii.colorize(:green)
+    puts 'Options:'
+    puts ''
     puts "1. Type a prompt for #{@selected_model} mentioning language to use `ex: write a ruby program that..`"
-    puts "2. Type `help` to show this message"
-    puts "3. Type `exit` to exit"
-    puts ""
+    puts '2. Type `help` to show this message'
+    puts '3. Type `exit` to exit'
+    puts ''
   end
 
   private
 
   def detect_language(message)
     message = message&.strip
-    # Check for shebang lines, which are a strong indicator of script language
     if message&.start_with?('#!')
       return 'bash' if message&.include?('bash') || message&.include?('sh')
       return 'python' if message&.include?('python')
@@ -248,31 +283,29 @@ class EvalGPT
       'text'
     end
   end
-  
+
   def print_two_columns(items)
-    items.each_slice(2).with_index(1) do |(item1, item2), index|
+    items.each_slice(2).with_index(1) do |(item1, item2), _index|
       puts "#{items.index(item1)}.#{item1}\t\t\t\t\t\t#{items.index(item2)}.#{item2}"
     end
   end
-  
+
   def clear_screen
     puts "\e[H\e[2J"
   end
-  
+
   def extract_code(response)
     matches = response.scan(/(\*\*(.*?)\*\*)\n```.*?\n?([\s\S]*?)\n```/m)
     files = {}
     puts "matches: #{matches}"
-  
+
     matches.each do |match|
       files[match[1]] = match[2].strip
     end
-  
+
     puts "files: #{files}"
     files
   end
-  
-
 
   def create_directory_if_not_exists(relative_path)
     absolute_path = File.expand_path(relative_path)
@@ -283,20 +316,17 @@ class EvalGPT
   def select_model
     models = get_models
     clear_screen
-    @selected_model =  MODEL
+    @selected_model = MODEL
   end
 
   def get_models
-    begin
-      response = RestClient.get('https://api.openai.com/v1/models', @headers)
-      parsed_response = JSON.parse(response)
-      parsed_response = parsed_response['data']
-     
-      parsed_response.map { |model| model['id'] }
-      
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = RestClient.get('https://api.openai.com/v1/models', @headers)
+    parsed_response = JSON.parse(response)
+    parsed_response = parsed_response['data']
+
+    parsed_response.map { |model| model['id'] }
+  rescue RestClient::ExceptionWithResponse => e
+    e.response
   end
 
   def call_chatgpt
@@ -314,7 +344,7 @@ class EvalGPT
       parsed_response['choices'].first['message']['content']
     rescue RestClient::ExceptionWithResponse => e
       e.response
-      puts "Error:" + " #{e.response}"
+      puts 'Error:' + " #{e.response}"
     end
   end
 
@@ -331,8 +361,12 @@ class EvalGPT
       API_URL,
       body: data.to_json,
       headers: @headers.merge('Content-Type' => 'application/json'),
-      response_block: lambda { |chunk, remaining_bytes, total_bytes|
-        parsed_chunk = JSON.parse(chunk.gsub('data: ', '')) rescue nil
+      response_block: lambda { |chunk, _remaining_bytes, _total_bytes|
+        parsed_chunk = begin
+          JSON.parse(chunk.gsub('data: ', ''))
+        rescue StandardError
+          nil
+        end
         if parsed_chunk&.key?('choices')
           result = "#{result}#{parsed_chunk['choices'].first['delta']['content']}"
           clear_screen
@@ -340,26 +374,23 @@ class EvalGPT
         end
       }
     )
-  
-    if response.status != 200
-      puts "Error: #{response.status} #{response.body}"
-    end
+
+    puts "Error: #{response.status} #{response.body}" if response.status != 200
     result
-  rescue => e
+  rescue StandardError => e
     puts "Error: #{e.message}"
   end
-  
 end
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: evalgpt.rb [options]"
-  
+  opts.banner = 'Usage: evalgpt.rb [options]'
+
   opts.on('-i', '--input INPUT', 'Input to send to prompt') do |i|
     options[:input] = i
   end
   opts.on('-o', '--output OUTPUT', 'Output folder') do |o|
     options[:output] = o
-  end  
+  end
   opts.on('-v', '--verbose', 'Run in verbose mode') do |v|
     options[:verbose] = v
   end
